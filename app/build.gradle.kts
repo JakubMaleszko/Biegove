6b4426bd0204140416d7679c230f13e2424d88eb
JakubMaleszko <jakub.maleszko@gmail.com>
2026-08-25 16:53:52 +0200

    Updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.jakubmaleszko.biegove"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
